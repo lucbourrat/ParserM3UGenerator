bbd2ad4c8b97cb70259733f19d182e5c790907c2
luc <luc.bourrat.42@gmail.com>
2024-02-03 20:52:55 +0100

    code cleanning

--- script.js
+++ script.js
@@ -1,69 +1,93 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const m3uFileInput = document.getElementById('m3uFileInput');
-    const parseButton = document.getElementById('parseButton');
-    const preselectButtons = document.getElementsByClassName('preselectButton');
-    const groupTitlesList = document.getElementById('groupTitlesList');
-    const generateAndDownloadButton = document.getElementById('generateAndDownloadButton');
-
-    let m3uContent = '';
-
-    parseButton.addEventListener('click', () => {
-        const file = m3uFileInput.files[0];
-        if (file) {
-            const reader = new FileReader();
-
-            reader.onload = function(event) {
-                m3uContent = event.target.result;
-                const groupTitles = getUniqueGroupTitles(m3uContent);
-                displayGroupTitles(groupTitles);
-            };
-
-            reader.readAsText(file);
-        }
-    });
-
+document.addEventListener("DOMContentLoaded", () => {
+    const m3uFileInput = document.getElementById("m3uFileInput");
+    const parseButton = document.getElementById("parseButton");
+    const preselectButtons = document.getElementsByClassName("preselectButton");
+    const groupTitlesList = document.getElementById("groupTitlesList");
+    const generateAndDownloadButton = document.getElementById(
+        "generateAndDownloadButton"
+    );
+    let m3uContent = "";
     const languageStates = {};
-    for (const preselectButton of preselectButtons) {
-        preselectButton.addEventListener('click', (event) => {
-            const selectedLanguage = event.target.dataset.language;
-            const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-            
-            // Inverser l'état actuel
-            if (languageStates[selectedLanguage] === undefined) {
-                languageStates[selectedLanguage] = true;
-            } else {
-                languageStates[selectedLanguage] = !languageStates[selectedLanguage];
-            }
 
-            checkboxes.forEach(checkbox => {
-                const title = checkbox.value;
-                if (title.startsWith(`|${selectedLanguage}|`) || title.startsWith(`${selectedLanguage}|`) || title.startsWith(`|${selectedLanguage}`) || title.startsWith(`${selectedLanguage}`)) {
-                    checkbox.checked = languageStates[selectedLanguage];
-                }
-            });
+    setParseButton();
+    setPreselectButtons();
+    setGenerateAndDownloadButton();
 
-            if (languageStates[selectedLanguage]) {
-                event.target.textContent = `Unselect ${selectedLanguage}`;
-            } else {
-                event.target.textContent = `Preselect ${selectedLanguage}`;
+    function setParseButton() {
+        parseButton.addEventListener("click", () => {
+            const file = m3uFileInput.files[0];
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function (event) {
+                    m3uContent = event.target.result;
+                    const groupTitles = getUniqueGroupTitles(m3uContent);
+                    displayGroupTitles(groupTitles);
+                };
+
+                reader.readAsText(file);
             }
         });
     }
 
-    generateAndDownloadButton.addEventListener('click', () => {
-        const checkedTitles = [];
-        const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    function setPreselectButtons() {
+        for (const preselectButton of preselectButtons) {
+            preselectButton.addEventListener("click", (event) => {
+                const selectedLanguage = event.target.dataset.language;
+                const checkboxes = document.querySelectorAll(
+                    'input[type="checkbox"]'
+                );
 
-        checkboxes.forEach(checkbox => {
-            checkedTitles.push(checkbox.value);
+                // Inverser l'état actuel
+                if (languageStates[selectedLanguage] === undefined) {
+                    languageStates[selectedLanguage] = true;
+                } else {
+                    languageStates[selectedLanguage] =
+                        !languageStates[selectedLanguage];
+                }
+
+                checkboxes.forEach((checkbox) => {
+                    const title = checkbox.value;
+                    if (
+                        title.startsWith(`|${selectedLanguage}|`) ||
+                        title.startsWith(`${selectedLanguage}|`) ||
+                        title.startsWith(`|${selectedLanguage}`) ||
+                        title.startsWith(`${selectedLanguage}`)
+                    ) {
+                        checkbox.checked = languageStates[selectedLanguage];
+                    }
+                });
+
+                if (languageStates[selectedLanguage]) {
+                    event.target.textContent = `Unselect ${selectedLanguage}`;
+                } else {
+                    event.target.textContent = `Preselect ${selectedLanguage}`;
+                }
+            });
+        }
+    }
+
+    function setGenerateAndDownloadButton() {
+        generateAndDownloadButton.addEventListener("click", () => {
+            const checkedTitles = [];
+            const checkboxes = document.querySelectorAll(
+                'input[type="checkbox"]:checked'
+            );
+
+            checkboxes.forEach((checkbox) => {
+                checkedTitles.push(checkbox.value);
+            });
+
+            const newM3UContent = generateNewM3UContent(
+                m3uContent,
+                checkedTitles
+            );
+            downloadM3UFile(newM3UContent);
         });
-
-        const newM3UContent = generateNewM3UContent(m3uContent, checkedTitles);
-        downloadM3UFile(newM3UContent);
-    });
+    }
 
     function getUniqueGroupTitles(m3uContent) {
-        const m3uLines = m3uContent.split('\n');
+        const m3uLines = m3uContent.split("\n");
         const groupTitlesSet = new Set();
 
         for (let i = 1; i < m3uLines.length; i += 2) {
@@ -80,16 +104,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function displayGroupTitles(groupTitles) {
-        groupTitlesList.innerHTML = '';
+        groupTitlesList.innerHTML = "";
 
-        groupTitles.forEach(title => {
-            const listItem = document.createElement('li');
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
+        groupTitles.forEach((title) => {
+            const listItem = document.createElement("li");
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
             checkbox.value = title;
             listItem.appendChild(checkbox);
 
-            const label = document.createElement('label');
+            const label = document.createElement("label");
             label.textContent = title;
             listItem.appendChild(label);
 
@@ -98,24 +122,27 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function generateNewM3UContent(originalContent, titles) {
-        const originalLines = originalContent.split('\n');
-        let newContent = '';
+        const originalLines = originalContent.split("\n");
+        let newContent = "";
         let includeChannel = false;
-    
-        newContent += originalLines[0] + '\n'; // Ajoute la première ligne "#EXTM3U"
 
-        for (let i = 1; i < originalLines.length; i=i+2) {
+        newContent += originalLines[0] + "\n"; // Ajoute la première ligne "#EXTM3U"
+
+        for (let i = 1; i < originalLines.length; i = i + 2) {
             if (includeChannel) {
                 includeChannel = false;
             }
-    
-            if ((i - 1) % 2 === 0 && titles.includes(getGroupTitle(originalLines[i]))) {
+
+            if (
+                (i - 1) % 2 === 0 &&
+                titles.includes(getGroupTitle(originalLines[i]))
+            ) {
                 includeChannel = true;
-                newContent += originalLines[i] + '\n';
-                newContent += originalLines[i + 1] + '\n';
+                newContent += originalLines[i] + "\n";
+                newContent += originalLines[i + 1] + "\n";
             }
         }
-    
+
         return newContent;
     }
 
@@ -124,24 +151,24 @@ document.addEventListener('DOMContentLoaded', () => {
         if (match) {
             return match[1];
         }
-        return '';
+        return "";
     }
 
     function downloadM3UFile(content) {
-        const blob = new Blob([content], { type: 'text/plain' });
+        const blob = new Blob([content], { type: "text/plain" });
         const url = URL.createObjectURL(blob);
 
-        const a = document.createElement('a');
+        const a = document.createElement("a");
         a.href = url;
-        a.download = 'selected_channels.m3u';
-        a.textContent = 'Download M3U';
+        a.download = "selected_channels.m3u";
+        a.textContent = "Download M3U";
 
-        a.style.display = 'none';
+        a.style.display = "none";
         document.body.appendChild(a);
 
         a.click();
 
-        a.addEventListener('click', () => {
+        a.addEventListener("click", () => {
             setTimeout(() => {
                 document.body.removeChild(a);
                 URL.revokeObjectURL(url);
